301470cca6f7884f9ec2b5e3d285eb2bb4823882
KALPESH SOLANKI <184857390+xploitoverload@users.noreply.github.com>
2025-07-05 01:01:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,49 @@
+// src/App.tsx
 import { createContext, useEffect, useState } from "react";
 import { DefaultTheme, ThemeProvider } from "styled-components";
 import { useTheme } from "./hooks/useTheme";
 import GlobalStyle from "./components/styles/GlobalStyle";
 import Terminal from "./components/Terminal";
+import { defaultTheme } from "./components/styles/themes"; // Import defaultTheme
 
 export const themeContext = createContext<
   ((switchTheme: DefaultTheme) => void) | null
 >(null);
 
 function App() {
-  // themes
   const { theme, themeLoaded, setMode } = useTheme();
-  const [selectedTheme, setSelectedTheme] = useState(theme);
-
-  // Disable browser's default behavior
-  // to prevent the page go up when Up Arrow is pressed
+  const [selectedTheme, setSelectedTheme] = useState(defaultTheme); // Set to defaultTheme
+  
   useEffect(() => {
     window.addEventListener(
       "keydown",
-      e => {
-        ["ArrowUp", "ArrowDown"].indexOf(e.code) > -1 && e.preventDefault();
-      },
+      e => ["ArrowUp", "ArrowDown"].includes(e.code) && e.preventDefault(),
       false
     );
   }, []);
 
   useEffect(() => {
-    setSelectedTheme(theme);
+    if (themeLoaded) {
+      // Apply default theme if no preference is saved
+      const savedTheme = localStorage.getItem('theme');
+      if (!savedTheme) {
+        setMode(defaultTheme);
+      }
+      setSelectedTheme(theme);
+    }
   }, [themeLoaded]);
 
-  // Update meta tag colors when switching themes
   useEffect(() => {
-    const themeColor = theme.colors?.body;
-
+    const themeColor = selectedTheme.colors?.body;
     const metaThemeColor = document.querySelector("meta[name='theme-color']");
     const maskIcon = document.querySelector("link[rel='mask-icon']");
     const metaMsTileColor = document.querySelector(
       "meta[name='msapplication-TileColor']"
     );
 
-    metaThemeColor && metaThemeColor.setAttribute("content", themeColor);
-    metaMsTileColor && metaMsTileColor.setAttribute("content", themeColor);
-    maskIcon && maskIcon.setAttribute("color", themeColor);
+    metaThemeColor?.setAttribute("content", themeColor);
+    metaMsTileColor?.setAttribute("content", themeColor);
+    maskIcon?.setAttribute("color", themeColor);
   }, [selectedTheme]);
 
   const themeSwitcher = (switchTheme: DefaultTheme) => {
@@ -51,9 +53,7 @@ function App() {
 
   return (
     <>
-      <h1 className="sr-only" aria-label="Terminal Portfolio">
-        Terminal Portfolio
-      </h1>
+      <h1 className="sr-only">Terminal Portfolio</h1>
       {themeLoaded && (
         <ThemeProvider theme={selectedTheme}>
           <GlobalStyle />
