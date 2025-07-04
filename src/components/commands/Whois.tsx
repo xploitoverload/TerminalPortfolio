a2a84dd965a6d57bcd02f2ef66491c639a9ec46e
KALPESH SOLANKI <184857390+xploitoverload@users.noreply.github.com>
2025-07-04 13:07:52 +0000

    modified

--- src/components/commands/Whois.tsx
+++ src/components/commands/Whois.tsx
@@ -1,49 +1,51 @@
 import {
-  AboutWrapper,
-  HighlightAlt,
-  HighlightSpan,
+    AboutWrapper,
+    HighlightAlt,
+    HighlightSpan,
 } from "../styles/About.styled";
 import Type from "../Type";
 import Sequence from "../Sequence";
 
 const Whois: React.FC = () => {
-  return (
-    <AboutWrapper data-testid="Whois">
-      <Sequence speed={25} gap={350}>
-        {/* line 1 */}
-        <p>
-          <Type>Hi, my name is </Type>
-          <HighlightSpan>KALPESH SOLANKI</HighlightSpan>
-          <Type>! You can also call me Xploitoverload.</Type>
-        </p>
+    return (
+        <AboutWrapper data-testid="Whois">
+            <Sequence speed={25} gap={350}>
+                {/* line 1 */}
+                <p>
+                    <Type>Hi, my name is </Type>
+                    <HighlightSpan>KALPESH SOLANKI</HighlightSpan>
+                    <Type>! You can also call me Xploitoverload.</Type>
+                </p>
 
-        {/* line 2 */}
-        <p>
-          <Type>
-            I'm <HighlightAlt>a security researcher</HighlightAlt> and{" "}
-            <HighlightAlt>hacker</HighlightAlt>.
-          </Type>
-        </p>
+                {/* line 2 */}
+                <p>
+                    <Type>I'm </Type>
+                    <HighlightAlt>a security researcher</HighlightAlt>
+                    <Type> and </Type>
+                    <HighlightAlt>hacker</HighlightAlt>
+                    <Type>.</Type>
+                </p>
 
-        {/* line 3 */}
-        <p>
-          <Type>I love to build and hack stuff.</Type>
-        </p>
 
-        {/* line 4 */}
-        <p>
-          <Type>To see my projects please type "projects".</Type>
-        </p>
+                {/* line 3 */}
+                <p>
+                    <Type>I love to build and hack stuff.</Type>
 
-        {/* line 5 */}
-        <p>
-          <Type>
-            To learn more about me with a GUI portfolio, please type "gui".
-          </Type>
-        </p>
-      </Sequence>
-    </AboutWrapper>
-  );
+
+                    {/* line 4 */}
+
+                    <Type>To see my projects please type "projects".</Type>
+
+
+                    {/* line 5 */}
+
+                    <Type>
+                        To learn more about me with a GUI portfolio, please type "gui".
+                    </Type>
+                </p>
+            </Sequence>
+        </AboutWrapper>
+    );
 };
 
 export default Whois;
