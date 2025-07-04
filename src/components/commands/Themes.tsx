f854b6829bd7cab4fe79babb4d8b366abfa09663
KALPESH SOLANKI <184857390+xploitoverload@users.noreply.github.com>
2025-07-05 01:05:36 +0530

    Update Themes.tsx

--- src/components/commands/Themes.tsx
+++ src/components/commands/Themes.tsx
@@ -1,36 +1,30 @@
+// src/components/Terminal/commands/Themes.tsx
 import { useContext, useEffect } from "react";
 import _ from "lodash";
 import { themeContext } from "../../App";
 import { Wrapper } from "../styles/Output.styled";
 import { ThemeSpan, ThemesWrapper } from "../styles/Themes.styled";
-import {
-    checkThemeSwitch,
-    getCurrentCmdArry,
-    isArgInvalid,
-} from "../../utils/funcs";
+import { checkThemeSwitch, getCurrentCmdArry, isArgInvalid } from "../../utils/funcs";
 import { termContext } from "../Terminal";
-import theme from "../styles/themes";
+import theme, { defaultTheme } from "../styles/themes";
 import Usage from "../Usage";
 
 const myThemes = _.keys(theme);
 
 const Themes: React.FC = () => {
     const { arg, history, rerender } = useContext(termContext);
-
     const themeSwitcher = useContext(themeContext);
-
-    /* ===== get current command ===== */
     const currentCommand = getCurrentCmdArry(history);
 
-    /* ===== check current command makes redirect ===== */
     useEffect(() => {
-        if (checkThemeSwitch(rerender, currentCommand, myThemes)) {
+        if (currentCommand[0] === "themes" && currentCommand.length === 1) {
+            themeSwitcher?.(defaultTheme);
+        } else if (checkThemeSwitch(rerender, currentCommand, myThemes)) {
             themeSwitcher?.(theme[currentCommand[2]]);
         }
     }, [arg, rerender, currentCommand]);
 
-    /* ===== check arg is valid ===== */
-    const checkArg = () =>
+    const checkArg = () => 
         isArgInvalid(arg, "set", myThemes) ? <Usage cmd="themes" /> : null;
 
     return arg.length > 0 || arg.length > 2 ? (
