9195ed818133b5cf94d9a567afdc9a9cc0244046
KALPESH SOLANKI <184857390+xploitoverload@users.noreply.github.com>
2025-07-04 13:18:36 +0000

    modified

--- src/components/commands/Whois.tsx
+++ src/components/commands/Whois.tsx
@@ -1,51 +1,45 @@
 import {
-    AboutWrapper,
-    HighlightAlt,
-    HighlightSpan,
+  AboutWrapper,
+  HighlightAlt,
+  HighlightSpan,
 } from "../styles/About.styled";
 import Type from "../Type";
 import Sequence from "../Sequence";
 
 const Whois: React.FC = () => {
-    return (
-        <AboutWrapper data-testid="Whois">
-            <Sequence speed={25} gap={350}>
-                {/* line 1 */}
-                <p>
-                    <Type>Hi, my name is </Type>
-                    <HighlightSpan>KALPESH SOLANKI</HighlightSpan>
-                    <Type>! You can also call me Xploitoverload.</Type>
-                </p>
+  return (
+    <AboutWrapper data-testid="Whois">
+      <Sequence speed={25} gap={350}>
+        <div style={{ marginBottom: "4px" }}>
+          <Type>Hi, my name is </Type>
+          <HighlightSpan>KALPESH SOLANKI</HighlightSpan>
+          <Type>! You can also call me Xploitoverload.</Type>
+        </div>
 
-                {/* line 2 */}
-                <p>
-                    <Type>I'm </Type>
-                    <HighlightAlt>a security researcher</HighlightAlt>
-                    <Type> and </Type>
-                    <HighlightAlt>hacker</HighlightAlt>
-                    <Type>.</Type>
-                </p>
+        <div style={{ marginBottom: "4px" }}>
+          <Type>I'm </Type>
+          <HighlightAlt>a security researcher</HighlightAlt>
+          <Type> and </Type>
+          <HighlightAlt>hacker</HighlightAlt>
+          <Type>.</Type>
+        </div>
 
+        <div style={{ marginBottom: "4px" }}>
+          <Type>I love to build and hack stuff.</Type>
+        </div>
 
-                {/* line 3 */}
-                <p>
-                    <Type>I love to build and hack stuff.<br></br></Type>
+        <div style={{ marginBottom: "4px" }}>
+          <Type>To see my projects please type "projects".</Type>
+        </div>
 
-
-                    {/* line 4 */}
-
-                    <Type>To see my projects please type "projects".<br></br></Type>
-
-
-                    {/* line 5 */}
-
-                    <Type>
-                        To learn more about me with a GUI portfolio, please type "gui".
-                    </Type>
-                </p>
-            </Sequence>
-        </AboutWrapper>
-    );
+        <div style={{ marginBottom: "4px" }}>
+          <Type>
+            To learn more about me with a GUI portfolio, please type "gui".
+          </Type>
+        </div>
+      </Sequence>
+    </AboutWrapper>
+  );
 };
 
 export default Whois;
